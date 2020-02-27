f2b5a931ab102c9d6b1e49241bf2213fa1a7d3f7
Paul Santora <45446704+leavepaulS@users.noreply.github.com>
2020-02-27 14:14:17 -0500

    Add files via upload

--- my_timer.c
+++ my_timer.c
@@ -11,26 +11,37 @@ MODULE_LICENSE("Dual BSD/GPL");
 #define BUF_LEN 100
 
 static struct proc_dir_entry* proc_entry;
-static long time;
+struct timespec time;
+struct timespec time2;
+static int second = 0;
 
-static char msg[BUF_LEN];
 static int procfs_buf_len;
 static char *message1;
 
 static ssize_t procfile_read(struct file* file, char * ubuf, size_t count, loff_t *ppos)
 {
-	printk(KERN_INFO "current time: %ld\n", current_kernel_time());
-        //printf(msg,"current time: %ld\n", current_kernel_time());
-	message1 = kmalloc(sizeof(char) * 256,__GFP_RECLAIM | __GFP_IO | __GFP_FS);
-	time = current_kernel_time().tv_sec;
+	if (second == 0)
+	{
+		message1 = kmalloc(sizeof(char) * 256,__GFP_RECLAIM | __GFP_IO | __GFP_FS);
+		time = current_kernel_time();
+		sprintf(message1,"current time: %ld\n", time);
+	}
+	else
+	{
+		message1 = kmalloc(sizeof(char) * 256,__GFP_RECLAIM | __GFP_IO | __GFP_FS);
+		time2 = current_kernel_time();
+		sprintf(message1,"elapsed time: %ld.%ld\n", time2.tv_sec - time.tv_sec, time2.tv_nsec - time.tv_nsec);
+		time = current_kernel_time();
+	}
+	++second;
 	
-	sprintf(message1,"the current time: %ld\n", time);
+	
 	procfs_buf_len = strlen(message1);
         
 	if (*ppos > 0 || count < procfs_buf_len)
 		return 0;
 
-	if (copy_to_user(ubuf,message1, procfs_buf_len))
+	if (copy_to_user(ubuf, message1, procfs_buf_len))
 		return -EFAULT;
 
 	*ppos = procfs_buf_len;
@@ -39,32 +50,11 @@ static ssize_t procfile_read(struct file* file, char * ubuf, size_t count, loff_
 
 	return procfs_buf_len;
 }
-
-
-static ssize_t procfile_write(struct file* file, const char * ubuf, size_t count, loff_t* ppos)
-{
-	printk(KERN_INFO "proc_write\n");
-
-	if (count > BUF_LEN)
-		procfs_buf_len = BUF_LEN;
-	else
-		procfs_buf_len = count;
-
-	copy_from_user(msg, ubuf, procfs_buf_len);
-
-	printk(KERN_INFO "got from user: %s\n", msg);
-
-	return procfs_buf_len;
-}
-
-
 static struct file_operations procfile_fops = {
 	.owner = THIS_MODULE,
 	.read = procfile_read,
-	.write = procfile_write,
 };
-
-static int hello_init(void)
+static int timer_init(void)
 {
 	proc_entry = proc_create("timed", 0666, NULL, &procfile_fops);
 
@@ -73,15 +63,10 @@ static int hello_init(void)
 	
 	return 0;
 }
-
-static void hello_exit(void)
+static void timer_exit(void)
 {
 	proc_remove(proc_entry);
 	return;
 }
-
-
-
-module_init(hello_init);
-module_exit(hello_exit);
-
+module_init(timer_init);
+module_exit(timer_exit);
