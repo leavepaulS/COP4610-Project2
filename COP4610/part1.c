94662b162fe7b8e69c131f8479b4e5f5b60f67d5
Paul Santora <45446704+leavepaulS@users.noreply.github.com>
2020-03-04 15:20:07 -0500

    Add files via upload

---
+++ COP4610/part1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+    const char mesg[] = "Hello";
+    // first system call
+    syscall(4, STDOUT_FILENO, mesg, sizeof(mesg)-1);
+    printf("Hello World\n");
+    int i =0;
+
+    return 0;
+}
